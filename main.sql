d140b34fb5ae7ae5c53d9d6bbb319e97782ba091
Kinie K Kusuma <66422665+kinkusuma@users.noreply.github.com>
2022-03-18 18:45:06 +0800

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,14 @@
+SELECT
+    posts.id,
+    posts.text,
+    posts.user_id,
+    posts.created_at,
+    posts.updated_at,
+    COUNT(post_likes.post_id) AS total_likes
+FROM
+    posts
+INNER JOIN post_likes ON posts.user_id = post_likes.user_id
+WHERE
+    posts.user_id = 10
+GROUP BY
+    posts.user_id
